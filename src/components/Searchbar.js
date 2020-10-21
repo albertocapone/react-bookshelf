47cad7fee037fbb0a758e634ad3e03ecafde584d
Alberto Capone <albertocapone@outlook.it>
2020-10-21 17:39:56 +0200

    Improved Live Search
    - implemented request cancellation
    - fixed synchronization issue on input

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,14 +2,31 @@ import React from 'react';
 import axios from 'axios';
 
 const Searchbar = (props) => {
-    
+  
+    const CancelToken = axios.CancelToken;
+    let cancel;
+
     const handleAjaxCall = (e) => {
+
+      if(cancel) cancel();    
+
       const query = e.target.value;
+
+      if(!query.trim()) {               //this block is useful to clean searchResults when user presses and holds the delete button           
+        setTimeout( () => {
+          props.handleResultsUpdate([]);
+        }, 1000);
+        return;
+      }
+
       const API = `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=40`; //added parameter |maxResults=40| to get as many results as possible
       axios
-        .get(API)
+        .get(API, { 
+          cancelToken: new CancelToken( (c) => { cancel = c; } ) 
+        })
         .then((res) => {
-          const data = res.data.items ? res.data.items : [];
+          const data = res.data.items ? res.data.items : [];  //sometimes, even on success, API responds with no items
+          console.log(res.data);
           props.handleResultsUpdate(data);
         })
         .catch((err) => {
