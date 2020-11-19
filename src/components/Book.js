850cc575d4dee9be03e2d1e139021faae8c19145
Alberto Capone <albertocapone@outlook.it>
2020-11-19 21:38:18 +0100

    <Book /> refactoring with hooks

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,41 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ClampLines from "react-clamp-lines";
 import noImg from "../img/no-img.jpg";
 
 
-class Book extends React.Component {
+const Book = (props) => {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            hover: false
-        };
+    const [hover, setHover] = useState(false);
+
+    const handleModalOpening = () => {
+      props.handleDetailsModalUpdate(true, props.book);
     }
 
-    handleModalOpening = () => {
-      this.props.handleDetailsModalUpdate(true, this.props.book);
+    const handleHover = (e) => {
+        const hovering = (e.type === 'mouseenter')? true : false;
+        setHover(hovering);
     }
 
-    handleHover = (e) => {
-        const hover = (e.type === 'mouseenter')? true : false;
-        this.setState( () => ( { hover } ) );
-    }
-
-    render() {
-        return (
-            <li onMouseEnter={this.handleHover} onMouseLeave={this.handleHover} onClick={this.handleModalOpening} className={`media pt-2 pb-2 pl-2 border-bottom ${ (this.state.hover) ? 'bg-hover' : ''}`} style={{height: "80px"}}>
-                <img src={ (this.props.book.imageLinks) ? this.props.book.imageLinks.smallThumbnail : noImg } className="mr-3 h-100 rounded" alt="..." />
-                <ClampLines
-                    text={this.props.book.title}
-                    lines={2}
-                    ellipsis="[...]"
-                    buttons={false}
-                    className="media-body"  //<div class=media-body> will be the parent
-                    innerElement="h6"       //<h6> clamped-title </h6> will be its child node
-                />
-            </li>
-        );
-    }
+    return (
+        <li onMouseEnter={handleHover} onMouseLeave={handleHover} onClick={handleModalOpening} className={`media pt-2 pb-2 pl-2 border-bottom ${ (hover) ? 'bg-hover' : ''}`} style={{height: "80px"}}>
+            <img src={ (props.book.imageLinks) ? props.book.imageLinks.smallThumbnail : noImg } className="mr-3 h-100 rounded" alt="..." />
+            <ClampLines
+                text={props.book.title}
+                lines={2}
+                ellipsis="[...]"
+                buttons={false}
+                className="media-body"  //<div class=media-body> will be the parent
+                innerElement="h6"       //<h6> clamped-title </h6> will be its child node
+            />
+        </li>
+    );
 }
 
 export default Book;
